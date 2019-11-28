fa4de416b6e314a69604d946c25a0852d88d83b3
ZibaloVL <41912546+ZibaloVL@users.noreply.github.com>
2019-11-28 06:26:12 +0200

    20.26 connect in db

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser'); 
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const methodOverride = require('method-override');
 
 const app = express();
 
+// Middlware
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
+
+// Mongo URI
+const mongoUri = `mongodb+srv://ZibaloVL1971:baracudaZibalo@img-rzvtw.mongodb.net/test`
+// Create mongo connections
+const conn = mongoose.createConnection(mongoUri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.get('/', (req, res) => {
     res.render('index')
